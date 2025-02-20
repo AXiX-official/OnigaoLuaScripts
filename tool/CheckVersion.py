163ab3567275d8685a4b69a483f14f84c859f3e1
AXiX-official <2879710747@qq.com>
2025-02-20 23:41:08 +0800

    Fix Script

--- tool/CheckVersion.py
+++ tool/CheckVersion.py
@@ -25,11 +25,12 @@ if __name__ == '__main__':
     if not os.path.exists("version.txt"):
         result = f"{LatestGameVersion}-{InternalResourceVersion}"
     else:
-        # check difference bitween version.txt and version-new.txt
         with open("version.txt") as f:
             old_version = json.load(f)
         if old_version["LatestGameVersion"] == VersionData["LatestGameVersion"] and old_version["InternalResourceVersion"] == VersionData["InternalResourceVersion"]:
             result = None
         else:
             result = f"{LatestGameVersion}-{InternalResourceVersion}"
-    print(f"::set-output name=version::{result}")
+
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        print(f"version={result}", file=fh)
